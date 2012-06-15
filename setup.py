201526135f25af2926a0ad06a1e1bdef7545e079
Petr Bondarenko <public@shamanis.com>
2012-06-15 18:21:00 +0600

    Fix: setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__FILE__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='python-daemon',
